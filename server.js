2e12b7427795f18d4740c823e48e381802a562a9
Nathan Sartain <nathanmsartain@gmail.com>
2020-02-10 14:21:29 -0500

    Added POST /api/notes route.

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const dbString = fs.readFileSync('./db/db.json', 'utf-8');
-console.log(dbString);
-
 const app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+console.log(typeof(notes));
 
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
@@ -23,8 +23,16 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-  return res.json(dbString);
+  return res.json(notes);
 });
+
+app.post("/api/notes", function(req, res) {
+  notes.push(req.body);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+  res.json(true);
+});
+
+// DELETE /api/notes/:id
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
