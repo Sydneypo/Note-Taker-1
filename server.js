3428076c6e73573536bc231975e576d508415598
Nathan Sartain <nathanmsartain@gmail.com>
2020-02-10 11:27:58 -0500

    Added /notes route.

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.listen(PORT, function() {
