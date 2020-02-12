c77d03291b2406c8ed01c67e2e14139d030a0994
Nathan Sartain <nathanmsartain@gmail.com>
2020-02-12 13:23:18 -0500

    Added unique IDs to notes.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid/v4');
 
 const app = express();
 var PORT = process.env.PORT || 3000;
@@ -12,28 +13,38 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
-console.log(typeof(notes));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
-app.get("/api/notes", function(req, res) {
+app.get('/api/notes', (req, res) => {
   return res.json(notes);
 });
 
-app.post("/api/notes", function(req, res) {
-  notes.push(req.body);
+app.post('/api/notes', (req, res) => {
+  notes.push({
+    id: uuid(),
+    title: req.body.title,
+    text: req.body.text
+  });
+  console.log(notes);
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(true);
 });
 
-// DELETE /api/notes/:id
+// app.get('/api/notes/:id', (req, res) => {
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+// });
+
+// app.delete('/api/notes/:id', (req, res) => {
+
+// });
+
+app.listen(PORT, () => {
+  console.log('App listening on PORT ' + PORT);
 });
